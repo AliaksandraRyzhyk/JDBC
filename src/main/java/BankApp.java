5e50dc88654ca29f1c502ee9abdb0dd220e36663
Aliaksandra <ryzhikai@rambler.ru>
2021-08-23 11:42:51 +0300

    Commit

---
+++ src/main/java/BankApp.java
@@ -0,0 +1,17 @@
+
+public class BankApp {
+
+
+    public static void main(String[] args) {
+        AccountLogic accountLogic = new AccountLogic();
+        UserLogic userLogic = new UserLogic();
+
+        User user = userLogic.chooseOrCreateUser(userLogic.anyUserExists());
+
+        Account account = accountLogic.chooseOrCreateAccount(user);
+
+        account.operationsWithAccount();
+    }
+}
+
+
